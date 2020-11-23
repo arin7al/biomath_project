4aa7fe4c9c61166dd39b534bae341717d714cbbc
arin7al <2522634github>
2020-11-23 17:31:16 +0300

    trying to find out something about u*, v*

--- task1/jacobian_comp.py
+++ task1/jacobian_comp.py
@@ -2,9 +2,12 @@ import sympy as sp
 
 # sp.init_printing()
 
-k1 = sp.symbols('k1')
-k2 = sp.symbols('k2')
-k3 = sp.symbols('k3')
+# k1 = sp.symbols('k1')
+# k2 = sp.symbols('k2')
+# k3 = sp.symbols('k3')
+k1 = 1
+k2 = -1
+k3 = 1
 
 # v = sp.symbols('v')
 # u = sp.symbols('u')
@@ -17,20 +20,22 @@ v = (1 + u)/(k1 * u)
 #         -k1*u**2/(1+u),
 #     ],
 #     [
-#         k2*k3*v*(2*u + u**2)/(1+u)**2,
+#         -k2*k3*v*(2*u + u**2)/(1+u)**2,
 #         k2*(1 - k3*u**2/(1+u)),
 #         ]
 # ])
 # sp.pretty_print(M)
 lam = sp.symbols('lambda')
-# cp = sp.det(M - lam * sp.eye(2))
-# eigs = sp.roots(sp.Poly(cp, lam))
-# print eigs
 
-f1 = k3*u**2/(1+u) + k1*v/(1+u)**2 - 2
+eigs = sp.solve((1 - k1*v*(1/(1+u)**2) - lam)*(k2*(1 - k3*u**2/(1+u)) - lam) - (-k1*u**2/(1+u))*(-k2*k3*v*(2*u + u**2)/(1+u)**2), lam)
 
-f2 = 1 - k1*v/(1+u)**2 - k3*u**2/(1+u) + k1*k3*v*u**2/(1+u)**3 + k1*k3*u**3*(2+u)/(1+u)**3
-# eigs = sp.roots(sp.Poly(lam**2 + lam*f1 + f2, lam))
-eigs = sp.solve(lam**2 + lam*f1 + f2, lam)
-print eigs
 
+
+print eigs[0].evalf()
+print eigs[1].evalf()
+
+# print sp.expand((1 - k1*v*(1/(1+u)**2) - lam)*(k2*(1 - k3*u**2/(1+u)) - lam) - (-k1*u**2/(1+u))*(-k2*k3*v*(2*u + u**2)/(1+u)**2))
+
+# print sp.print_latex((-1 - k2 + k1*v/(u**2 +2*u + 1) + k2*k3*u*u/(u+1)))
+
+# sp.print_latex(sp.expand(-4*(-k1*k2*k3*u**4*v/(u**3 + 3*u**2 + 3*u + 1) - 2*k1*k2*k3*u**3*v/(u**3 + 3*u**2 + 3*u + 1) + k1*k2*k3*u**2*v/(u**3 + 3*u**2 + 3*u + 1) - k1*k2*v/(u**2 + 2*u + 1 )- k2*k3*u**2/(u + 1)+ k2) + (k1*v/(u**2 + 2*u + 1) + k2*k3*u**2/(u + 1)  - k2 - 1)**2))
